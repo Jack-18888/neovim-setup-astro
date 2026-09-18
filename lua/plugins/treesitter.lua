ffe02e1b572275bb1a636d474d56ab7a9027436a
Jack-18888 <jack.shi.smart@gmail.com>
2026-09-17 22:16:23 -0400

    disable treesitter auto_install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 -- --------------------
 -- Treesitter customizations are handled with AstroCore
@@ -11,14 +9,7 @@ return {
   ---@type AstroCoreOpts
   opts = {
     treesitter = {
-      highlight = true, -- enable/disable treesitter based highlighting
-      indent = true, -- enable/disable treesitter based indentation
-      auto_install = true, -- enable/disable automatic installation of detected languages
-      ensure_installed = {
-        "lua",
-        "vim",
-        -- add more arguments for adding more treesitter parsers
-      },
+      auto_install = false, -- enable/disable automatic installation of detected languages
     },
   },
 }
